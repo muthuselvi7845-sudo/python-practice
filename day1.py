9da784d471f1c1f8c88e8473654d7411e637d266
muthuselvi7845-sudo <muthuselvi7845@gmail.com>
2026-03-04 11:32:04 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,75 @@
+#find the 10 numers and of their sum
+'''a=[]
+print("Enter 10 Numbers")
+for i in range(1,11):
+    num=int(input("Enter a number "+str(i)))
+    a.append(num)
+print(a)
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)'''
+
+
+
+#find the 10 numbers and of their average
+'''a=[]
+print("Enter 10 Numbers")
+for i in range(1,11):
+    num=int(input("Enter a number "+str(i)))
+    a.append(num)
+print(a)
+avg=0
+for i in a:
+    avg=(avg+i)/10
+print(avg)'''
+
+
+#find sum of the first 5 number
+'''sum=0
+for i in range(1,6):
+    sum=sum+i
+print(sum)'''
+
+#find the n terms of natural numbers and  heir sum (test data:7)
+'''n=int(input("Enter a number"))
+sum=0
+print("the natural numbers are")
+for i in range(1,n+1):
+    print(i)
+    sum=sum+i
+print("sum of the natural numbers",sum)'''
+
+
+
+#Find the cube of number and an integer
+'''n=int(input("Enter a number "))
+for i in range(1,n+1):
+    print("The number is",i,"The cube of the",i,"is:",i*i*i)'''
+
+
+#Print first n multiples of 5.
+'''n=int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(5*i)'''
+
+
+#Find the product of first n natural numbers.
+'''n=int(input("Enter a number"))
+product=1
+for i in range(1,n+1):
+    print(i)
+    product=product*i
+print("the product of natural numbers:",product)'''
+
+
+#Print numbers from 1 to n in reverse order.
+'''n=int(input("Enter a number"))
+for i in range(n,0,-1):
+    print(i)'''
+
+
+
+
+
+
