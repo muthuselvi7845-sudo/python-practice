196c92d44490e945512abed0432d5dafdeca73e7
muthuselvi7845-sudo <muthuselvi7845@gmail.com>
2026-03-10 10:11:08 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,55 @@
+#Count how many numbers between 1 to n are even.
+'''n=int(input("Enter a number "))
+c=0
+for i in range(1,n+1):
+    if i%2==0:
+        c=c+1
+print("Even numbers are ",c)'''
+
+
+#Find the sum of digits of a given number.
+'''n=(input("Enter a number "))
+sum=0
+for i in n:
+    sum=sum+int(i)
+print("Sum of digits",sum)'''
+
+
+#count the number which are divisible by 3 and 5.
+'''c=0
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        print(i)
+        c=c+1
+print("count",c)'''
+
+
+#  1
+#  12
+#  123
+#  1234 
+#  Give me a input
+'''for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()'''
+
+
+#  *
+#  **
+#  ***
+#  ****
+#  give me as input
+'''for i in range(1,5):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()'''
+
+
+
+
+
+
+
+
+    
