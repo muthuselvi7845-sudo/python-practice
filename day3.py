1e67fd3e2066142bbbc98c93009a20bb929fd322
muthuselvi7845-sudo <muthuselvi7845@gmail.com>
2026-03-11 15:01:27 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,16 @@
+# pyramid model diamond shape * 
+n=int(input("Enter a number "))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(2*i-2):
+        print("*",end="")
+    print()
+
+for i in range(n-1,0,-1):
+     for j in range(n-i):
+          print(" ",end="")
+     for k in range(2*i-2):
+          print("*",end="")
+     print()
+     
