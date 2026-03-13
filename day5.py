1f595fa765ff9d16db82b5b36382f8d2b2e9441e
muthuselvi7845-sudo <muthuselvi7845@gmail.com>
2026-03-13 09:38:37 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,42 @@
+# Write a program to print 10 natural numbers in reverse order
+'''n=10
+while(n>=1):
+    print(n,end=", ")
+    n=n-1'''
+
+
+#write a program to find the factorial number
+#  (using while loop)
+'''n=int(input("enter a number:"))
+fact=1
+i=0
+while(i>0):
+    fact=fact*i
+    i=i+1
+print(fact)'''
+
+
+#write a program to find the factorial number
+#using for loop
+
+'''n=int(input("enter a number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+    print(fact)'''
+
+# write a program to  find the factorial number
+# using for loop
+'''def factorial(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    print(fact)
+n=int(input("Enter a number"))
+(factorial(n))'''
+
+
+
+
+
+
