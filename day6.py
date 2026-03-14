367697ecb7107e9600318cc528fd9d1c81bfef6c
muthuselvi7845-sudo <muthuselvi7845@gmail.com>
2026-03-14 14:11:33 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,25 @@
+#Function 
+# using function write a program to evenodd
+'''def even_odd(n):
+    if n%2==0:
+        print("Even")
+    else:
+        print("Odd")
+n=int(input("Enter the number"))
+even_odd(n)'''
+
+# using function write a program to passfail
+'''def pass_fail(m):
+    if m>=35:
+        print("pass")
+    else:
+        print("fail")
+n=int(input("Enter mark"))
+pass_fail(n)'''
+
+#printrange() let the fumction print numbers from a to b
+'''def printrange(n):
+    for i in range(1,n+1):
+        print(i)
+n=int(input("Enter a number"))
+printrange(n)'''
